2edc590654b3d90f81e3c24d6767135fe2a131f2
Gatovsky <101161255+hharieta@users.noreply.github.com>
2024-09-25 02:13:53 -0500

    Fixed prerrender page by tag filter

--- src/utils/getPostsByTag.ts
+++ src/utils/getPostsByTag.ts
@@ -1,10 +1,22 @@
 import type { CollectionEntry } from "astro:content";
 import getSortedPosts from "./getSortedPosts";
-import { slugifyAll } from "./slugify";
+import { slugifyAll, slugifyStr } from "./slugify";
 
-const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-  getSortedPosts(
-    posts.filter(post => slugifyAll(post.data.tags).includes(tag))
+const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) => {
+
+  const slugifiedTag = slugifyStr(tag);
+
+  const filteredPosts = posts.filter(
+    post => {
+      const tags = post.data.tags as string[];
+      return slugifyAll(tags).includes(slugifiedTag);
+    }
   );
+
+  const sortedPosts = getSortedPosts(filteredPosts);
+
+  return sortedPosts;
+};
+
 
 export default getPostsByTag;
